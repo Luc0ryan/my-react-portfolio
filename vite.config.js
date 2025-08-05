93d0e55a45b888fca9d610b283e68d07959b848b
Luc0ryan <lucryan@gmail.com>
2025-08-04 19:14:16 -0700

    solved it! need to push everything through @theme

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss({
-      config: {
-        content: [
-          "./index.html",
-          "./src/**/*.{js,jsx,ts,tsx}",
-        ],
-        theme: {
-          extend: {
-            colors: {
-              debug: "#ff00ff",
-            },
-          },
-        },
-        plugins: [],
-      },
-    }),
+    tailwindcss(),
   ],
 })
