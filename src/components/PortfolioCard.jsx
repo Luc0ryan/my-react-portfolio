c6f3b21753cb46b4dc68e1ab094ac811bf5eb830
Luc0ryan <lucryan@gmail.com>
2025-12-05 16:52:31 -0800

    new zoom effect

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -10,8 +10,7 @@ export default function PortfolioSection({
       {/* Image Block with natural aspect ratio */}
       <div
         className={`overflow-hidden rounded-lg border border-neutral-300
-                    transition-transform duration-300
-                    hover:-translate-y-0.5 hover:shadow-sm ${color}`}
+                    transition-transform duration-300 ease-out hover:scale-[1.008] ${color}`}
       >
         <div className="aspect-[4/3] w-full">
           <img
@@ -26,6 +25,9 @@ export default function PortfolioSection({
       <div className="flex flex-col gap-1 px-1">
         <h3 className="text-lg md:text-xl font-crimson font-bold antialiased text-left">
           {title}
+          <span
+      class="absolute left-0 bottom-0 h-[1px] w-0 bg-current transition-all duration-300 ease-out group-hover:w-full"
+    ></span>
         </h3>
         <p className="text-sm md:text-md text-left leading-snug text-neutral-700">
           {description}
